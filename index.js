3112ada44b2bccee54cf3094a1783a24028ff10a
Aditya raj <adityaraj987456@gmail.com>
2025-03-11 21:06:00 +0530

    check age above 19

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const name="buttan sarkar"
 
 const name2="aditya"
+const age=24
+
 
 function sayname(name){
     console.log("My name is "+ name)
+} 
+
+function checkage(age){
+    return age>=19
 }
